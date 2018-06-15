ff944b099187847c2602e8e7b83402512c53ad3e
Bart Veneman <bartveneman@users.noreply.github.com>
2018-06-15 23:51:34 +0200

    Revert static site, it does not work behind proxy

--- components/global/site-footer.js
+++ components/global/site-footer.js
@@ -2,19 +2,19 @@ const sitemap = [{
 	title: 'Project Wallace',
 	links: [{
 		title: 'Explore',
-		url: ''
+		url: 'https://www.projectwallace.com/explore'
 	}, {
 		title: 'About',
-		url: ''
+		url: 'https://www.projectwallace.com/about'
 	}, {
 		title: 'Blog',
-		url: ''
+		url: 'https://www.projectwallace.com/blog'
 	}]
 }, {
 	title: 'Social',
 	links: [{
 		title: 'GitHub',
-		url: ''
+		url: 'https://github.com/projectwallace'
 	}]
 }]
 
@@ -22,10 +22,10 @@ export default () => (
 	<footer className="site-footer">
 		<div className="sitemap">
 			{sitemap.map(section => (
-				<nav className="sitemap__section">
+				<nav className="sitemap__section" key={section.title.toLowerCase().replace(' ', '-')}>
 					<strong className="sitemap__title">{section.title}</strong>
 					{section.links.map(link => (
-						<a className="sitemap__link" href={link.url}>{link.title}</a>
+						<a key={link.url} className="sitemap__link" href={link.url}>{link.title}</a>
 					))}
 				</nav>
 			))}
--- next.config.js
+++ next.config.js
@@ -1,35 +1,8 @@
-const Prismic = require('prismic.io')
-const PrismicDOM = require('prismic-dom')
-const marked = require('marked')
-const striptags = require('striptags')
-const {Predicates} = Prismic
-
-const linkresolver = require('./helpers/linkresolver')
-
-function mapDoc(doc) {
-  function prismicToMarkDownToHtml(prismicContent, linkresolver) {
-    return marked(
-      striptags(
-        PrismicDOM.RichText.asHtml(
-          prismicContent,
-          linkresolver
-        ),
-        [],
-        '\n'
-      )
-    )
-  }
-
-  return {
-    type: 'doc',
-    title: doc.rawJSON.title,
-    content: prismicToMarkDownToHtml(doc.rawJSON.content, linkresolver),
-    lastModified: doc.lastPublicationDate,
-    slug: doc.uid
-  }
-}
+const {NOW_URL} = process.env
+const {now: {alias}} = require('./package.json')
 
 module.exports = {
+  assetPrefix: NOW_URL ? `https://${alias}` : 'http://localhost:3000',
   webpack: (config) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
@@ -37,24 +10,5 @@ module.exports = {
     }
 
     return config
-  },
-  exportPathMap: async () => {
-    const api = await Prismic.api('https://project-wallace.cdn.prismic.io/api/v2')
-    const {results} = await api.query(
-      Predicates.at('document.type', 'doc'),
-      { 'orderings': '[my.doc.title]' }
-    )
-    const docs = results
-      .map(doc => mapDoc(doc))
-      .reduce((acc, curr) => {
-        acc[`/docs/${curr.slug}`] = {page: '/doc', query: {slug: curr.slug}}
-        return acc
-      }, {})
-
-    return {
-      '/': {page: '/'},
-      '/docs': {page: '/docs'},
-      ...docs
-    }
   }
 }
